9ec9d49b8ae9985b4a40ab6158e05cddd02e5a83
Aksharpatel06 <aksharpatel030306@gmail.com>
2024-07-08 20:34:49 +0530

    08-07-2024

--- lib/view/task_1/screen/matching/matching_screen.dart
+++ lib/view/task_1/screen/matching/matching_screen.dart
@@ -6,10 +6,60 @@ class MatchingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
+      appBar: AppBar(
+        title: Text('Matxhing '),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: List.generate(
+                  5,
+                  (index) => Draggable(
+                    feedback: Container(
+                      margin: EdgeInsets.all(10),
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.blue.shade100,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                    child: Container(
+                      margin: const EdgeInsets.all(10),
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.red.shade100,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Column(
+                children: List.generate(
+                  5,
+                  (index) => DragTarget(
+                    builder: (context, candidateData, rejectedData) => Container(
+                      height: 50,
+                      margin: EdgeInsets.all(10),
 
-        ],
+                      width: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade100,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
