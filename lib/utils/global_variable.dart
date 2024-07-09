aeffa10dc72acdffddae33839339b84779764cd8
Aksharpatel06 <aksharpatel030306@gmail.com>
2024-07-09 14:49:43 +0530

    08-07-2024

---
+++ lib/utils/global_variable.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+// task 1 global variable
+
+List frontImage = [
+  'asset/animals/bird_front.png',
+  'asset/animals/dog_front.png',
+  'asset/animals/got_front.png',
+  'asset/animals/jely_front.png',
+];
+
+List backImage = [
+  'asset/animals/bird_back.png',
+  'asset/animals/dog_back.png',
+  'asset/animals/got_back.png',
+  'asset/animals/jely_back.png',
+];
+
+List<Color> colorList = [
+  Colors.orange.shade100,
+  Colors.orange.shade100,
+  Colors.orange.shade100,
+  Colors.orange.shade100,
+];
+
+int wrongAnswer = 2;
--- lib/view/task_1/screen/matching/matching_screen.dart
+++ lib/view/task_1/screen/matching/matching_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../../../utils/global_variable.dart';
 
 class MatchingScreen extends StatelessWidget {
   const MatchingScreen({super.key});
@@ -7,59 +10,159 @@ class MatchingScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Matxhing '),
+        backgroundColor: const Color(0xffF9891F),
+        centerTitle: true,
+        title: Text(
+          'Matching game',
+          style: GoogleFonts.overpass(
+              color: Colors.white, fontWeight: FontWeight.bold),
+        ),
       ),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                children: List.generate(
-                  5,
-                  (index) => Draggable(
-                    feedback: Container(
-                      margin: EdgeInsets.all(10),
-                      height: 50,
-                      width: 50,
-                      decoration: BoxDecoration(
-                        color: Colors.blue.shade100,
-                        shape: BoxShape.circle,
-                      ),
-                    ),
-                    child: Container(
-                      margin: const EdgeInsets.all(10),
-                      height: 50,
-                      width: 50,
-                      decoration: BoxDecoration(
-                        color: Colors.red.shade100,
-                        shape: BoxShape.circle,
-                      ),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 100,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: List.generate(
+                    4,
+                    (index) => Row(
+                      children: [
+                        SizedBox(
+                            height: 110,
+                            child: Image.asset(
+                              frontImage[index],
+                              fit: BoxFit.cover,
+                            )),
+                        Draggable(
+                          data: index,
+                          feedback: Container(
+                            margin: const EdgeInsets.all(10),
+                            height: 50,
+                            width: 50,
+                            decoration: BoxDecoration(
+                              color: Colors.blue.shade100,
+                              shape: BoxShape.circle,
+                            ),
+                          ),
+                          child: Container(
+                            margin: const EdgeInsets.all(10),
+                            height: 50,
+                            width: 50,
+                            decoration: BoxDecoration(
+                              color: Colors.red.shade100,
+                              shape: BoxShape.circle,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
-              ),
-              Column(
-                children: List.generate(
-                  5,
-                  (index) => DragTarget(
-                    builder: (context, candidateData, rejectedData) => Container(
-                      height: 50,
-                      margin: EdgeInsets.all(10),
-
-                      width: 50,
-                      decoration: BoxDecoration(
-                        color: Colors.grey.shade100,
-                        shape: BoxShape.circle,
-                      ),
+                const Spacer(),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: List.generate(
+                    4,
+                    (index) => Row(
+                      children: [
+                        DragTarget(
+                          onAcceptWithDetails: (details) {
+                            if (wrongAnswer > 0 && details.data == index) {
+                              colorList[index] = Colors.blue.shade100;
+                            } else if (wrongAnswer > 0 &&
+                                details.data != index) {
+                              showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return AlertDialog(
+                                    title: const Text('Try again!'),
+                                    content:
+                                        Text('Your are $wrongAnswer chance !'),
+                                    actions: [
+                                      TextButton(
+                                          onPressed: () {
+                                            Navigator.pop(context);
+                                          },
+                                          child: const Text('Next')),
+                                    ],
+                                  );
+                                },
+                              );
+                              wrongAnswer--;
+                              print(index);
+                            } else {
+                              showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return AlertDialog(
+                                    title: const Text('SucessFully....!'),
+                                    actions: [
+                                      TextButton(
+                                          onPressed: () {
+                                            Navigator.pop(context);
+                                          },
+                                          child: const Text('Next')),
+                                    ],
+                                  );
+                                },
+                              );
+                            }
+                          },
+                          builder: (context, candidateData, rejectedData) =>
+                              Container(
+                            height: 50,
+                            margin: const EdgeInsets.all(10),
+                            width: 50,
+                            decoration: BoxDecoration(
+                              color: colorList[index],
+                              shape: BoxShape.circle,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 110,
+                          child: Image.asset(backImage[index]),
+                        ),
+                      ],
                     ),
                   ),
                 ),
-              ),
-            ],
-          ),
+              ],
+            ),
+          ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: const Text('Restart Games'),
+                actions: [
+                  TextButton(
+                      onPressed: () {
+                        wrongAnswer = 3;
+                        Navigator.pop(context);
+                      },
+                      child: const Text('Yes')),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text('no')),
+                ],
+              );
+            },
+          );
+        },
+        child: const Icon(Icons.restart_alt),
       ),
     );
   }
